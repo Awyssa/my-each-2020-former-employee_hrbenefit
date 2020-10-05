f9fd404fd75cedef01e3eb06561c0cbe5cb4d02b
Michael Ernest <m.hendersun@gmail.com>
2020-10-05 12:46:39 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,15 +1,22 @@
+#
+# collection = [1,2,3,4]
+#
+# def my_each(collection = [1,2,3,4])
+#   i = 0
+#   while i > 5
+#     i + 1
+#   end
+#     collection.map { |i| "This is number #{i}"  }
+#
+#     return collection
+# end
 
-collection = [1,2,3,4]
 
-def my_each(collection = [1,2,3,4])
+def my_each(collection)
   i = 0
-  while i > 5
-    i + 1
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
   end
-    collection.map { |i| "This is number #{i}"  }
-
-  return collection
+  collection
 end
-
-
-my_each()
