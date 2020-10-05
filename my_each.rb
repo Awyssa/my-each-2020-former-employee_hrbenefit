5185d4212d87f5ef5e61cf8566932db82790730c
Michael Ernest <m.hendersun@gmail.com>
2020-10-05 13:38:03 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,22 @@
-def my_each # put argument(s) here
-  # code here
+#
+# collection = [1,2,3,4]
+#
+# def my_each(collection = [1,2,3,4])
+#   i = 0
+#   while i > 5
+#     i + 1
+#   end
+#     collection.map { |i| "This is number #{i}"  }
+#
+#     return collection
+# end
+
+
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+  collection
 end
